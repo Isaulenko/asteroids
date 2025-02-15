e7a1c18c5b630e75a9821aec3e36f588ac8cdd1e
Isaulenko <andrey.isaulenko@gmail.com>
2025-02-15 23:43:05 +0300

    create screen

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import os
+os.environ["SDL_AUDIODRIVER"] = "dummy"  # Disable audio
+
+
+
 import pygame
+from pygame.locals import *
+
+
 from constants import *
 
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
